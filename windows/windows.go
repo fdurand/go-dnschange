2362c3bb7347ab035588a417e7f63f2d720d407b
Durand Fabrice <oeufdure@gmail.com>
2021-04-26 09:54:06 -0400

    Moved debug

--- windows/windows.go
+++ windows/windows.go
@@ -72,7 +72,7 @@ func (runner *runner) GetInterfaces() ([]NetworkInterface, error) {
 		if err != nil {
 			continue
 		}
-		spew.Dump(NetInterface)
+
 		if s == uint64(1) {
 			NetInterface.DhcpEnabled = true
 			s, _, err := k.GetStringsValue("DhcpDefaultGateway")
@@ -162,6 +162,7 @@ func (runner *runner) GetInterfaces() ([]NetworkInterface, error) {
 		if err != nil {
 			log.Println(err)
 		}
+		spew.Dump(NetInterface)
 		NetworkInterfaces = append(NetworkInterfaces, *NetInterface)
 	}
 
